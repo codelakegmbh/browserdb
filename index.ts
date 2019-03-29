bf4997f08236ea252e87c4d1b1f642ef90c27405
Jörn Neumeyer <joernneumeyer@protonmail.ch>
2019-03-29 13:30:55 +0100

    added index for module exposure

---
+++ index.ts
@@ -0,0 +1,10 @@
+import { LocalDatabse } from './src/local-database';
+import { LocalDatabaseCollection } from './src/local-database-collection';
+import { LocalDatabaseManager } from './src/local-database-manager';
+
+export {
+  LocalDatabse,
+  LocalDatabaseCollection,
+  LocalDatabaseManager,
+};
+
