e9a1103f5c19f9718dfa46e4c1e6f8e9ed45837b
Jörn Neumeyer <joernneumeyer@protonmail.ch>
2019-03-30 20:09:25 +0100

    implemented 'getCollection'

--- src/local-database.ts
+++ src/local-database.ts
@@ -1,8 +1,22 @@
+import { LocalDatabaseCollection } from ".";
+
 export class LocalDatabase {
+  private collections: LocalDatabaseCollection[] = [];
+
   constructor(private name: string) {
   }
 
   public getName() {
     return this.name;
+  }
+
+  public getCollection(name: string) {
+    const existingInstance = this.collections.filter(x => x.getName() === name)[0];
+    if (existingInstance) {
+      return existingInstance;
+    }
+    const newInstance = new LocalDatabaseCollection(name);
+    this.collections.push(newInstance);
+    return newInstance;
   }
 }
