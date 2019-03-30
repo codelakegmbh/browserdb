088bebd41408b6307a752963aab65318da59047d
Jörn Neumeyer <joernneumeyer@protonmail.ch>
2019-03-30 18:27:18 +0100

    implemented 'getDatabase' method according to tests

--- src/local-database-manager.ts
+++ src/local-database-manager.ts
@@ -1,3 +1,16 @@
+import {LocalDatabase} from './local-database';
+
 export class LocalDatabaseManager {
-  
+  private static readonly instances: LocalDatabase[] = [];
+
+  static getDatabase(name: string) {
+    const existingInstance = LocalDatabaseManager.instances.filter(x => x.getName() === name)[0];
+    if (existingInstance) {
+      return existingInstance;
+    }
+    const newInstance = new LocalDatabase(name);
+    LocalDatabaseManager.instances.push(newInstance);
+    return newInstance;
+  }
 }
+
