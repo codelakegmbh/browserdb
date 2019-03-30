c1616ef76a99962147852edec09a70c1964e582c
Jörn Neumeyer <joernneumeyer@protonmail.ch>
2019-03-30 17:13:16 +0100

    moved index file

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import { LocalDatabase } from './local-database';
+import { LocalDatabaseCollection } from './local-database-collection';
+import { LocalDatabaseManager } from './local-database-manager';
+
+export {
+  LocalDatabase,
+  LocalDatabaseCollection,
+  LocalDatabaseManager,
+};
+
