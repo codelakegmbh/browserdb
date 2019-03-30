b06787806dcb7a305556b662d039d165898d24be
Jörn Neumeyer <joernneumeyer@protonmail.ch>
2019-03-30 18:27:03 +0100

    added tests for the static 'getDatabase' method

--- test/local-database-manager.spec.ts
+++ test/local-database-manager.spec.ts
@@ -1,5 +1,29 @@
+import {LocalDatabaseManager} from '../src/local-database-manager';
+import {LocalDatabase} from '../src';
+
 describe('local-database-manager-tests', () => {
-  test('dummy-test', () => {
-    expect(true).toBeTruthy();
+  describe('getDatabase', () => {
+    test('returns a database instance', () => {
+      const database = LocalDatabaseManager.getDatabase('moinsen');
+      expect(database).toBeInstanceOf(LocalDatabase);
+    });
+
+    test('passes the provided database name to the database', () => {
+      const database = LocalDatabaseManager.getDatabase('foo');
+      expect(database.getName()).toBe('foo');
+    });
+
+    test('returns the same database instance on multiple calls', () => {
+      const database = LocalDatabaseManager.getDatabase('moinsen');
+      const databaseSecondCall = LocalDatabaseManager.getDatabase('moinsen');
+      expect(database).toBe(databaseSecondCall);
+    });
+
+    test('returns the same database instance on multiple calls after switching given database name', () => {
+      const database = LocalDatabaseManager.getDatabase('moinsen');
+      LocalDatabaseManager.getDatabase('foo');
+      const databaseThirdCall = LocalDatabaseManager.getDatabase('moinsen');
+      expect(database).toBe(databaseThirdCall);
+    });
   });
 });
