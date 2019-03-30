1781a4b99ac6bbba42911ddfaf05e5706ea5ed92
Jörn Neumeyer <joernneumeyer@protonmail.ch>
2019-03-30 20:08:39 +0100

    added test for name getting

--- test/local-database-collection.spec.ts
+++ test/local-database-collection.spec.ts
@@ -1,5 +1,10 @@
+import { LocalDatabaseCollection } from "../src";
+
 describe('local-database-collection-tests', () => {
-  test('dummy-test', () => {
-    expect(true).toBeTruthy();
+  describe('getName', () => {
+    test('returns the passed name', () => {
+      const collection = new LocalDatabaseCollection('foo');
+      expect(collection.getName()).toBe('foo');
+    })
   });
 });
