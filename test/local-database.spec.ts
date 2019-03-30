c71ba0808163724450c793bc970d5f00f9087c23
Jörn Neumeyer <joernneumeyer@protonmail.ch>
2019-03-30 20:09:12 +0100

    added tests for getCollection

--- test/local-database.spec.ts
+++ test/local-database.spec.ts
@@ -1,4 +1,4 @@
-import { LocalDatabase } from "../src/local-database";
+import {LocalDatabase, LocalDatabaseCollection} from '../src';
 
 describe('local-database-tests', () => {
   describe('getName', () => {
@@ -8,4 +8,34 @@ describe('local-database-tests', () => {
       expect(database.getName()).toEqual(databaseName);
     });
   });
+
+  describe('getCollection', () => {
+    let database: LocalDatabase;
+
+    beforeAll(() => {
+      database = new LocalDatabase('bla');
+    });
+
+    test('returns a proper collection', () => {
+      expect(database.getCollection('bar')).toBeInstanceOf(LocalDatabaseCollection);
+    });
+
+    test('returns the same collection on multiple calls with the same name', () => {
+      const collection: LocalDatabaseCollection = database.getCollection('bar');
+      const collectionSecondCall: LocalDatabaseCollection = database.getCollection('bar');
+      expect(collection).toBe(collectionSecondCall);
+    });
+
+    test('returns the same collection on multiple calls with the same name if an intermediate collection had another name', () => {
+      const collection: LocalDatabaseCollection = database.getCollection('foo');
+      database.getCollection('bar');
+      const collectionThirdCall: LocalDatabaseCollection = database.getCollection('foo');
+      expect(collection).toBe(collectionThirdCall);
+    });
+
+    test('returned collection uses the provided name', () => {
+      const collection: LocalDatabaseCollection = database.getCollection('bar');
+      expect(collection.getName()).toBe('bar');
+    });
+  })
 });
