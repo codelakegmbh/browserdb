ea3ba675d8476106749fc40573edd89730636fdf
Jörn Neumeyer <joernneumeyer@protonmail.ch>
2019-03-31 15:09:40 +0200

    added test sceleton for item insertion; added local storage mock setup

--- test/local-database-collection.spec.ts
+++ test/local-database-collection.spec.ts
@@ -1,10 +1,28 @@
 import { LocalDatabaseCollection } from "../src";
 
+function initializeLocalStorageMock() {
+  let storage = { };
+
+  spyOn(window.localStorage, 'setItem').and.callFake((key: string, value: string) => {
+    storage[key] = value;
+  });
+
+  spyOn(window.localStorage, 'getItem').and.callFake((key: string) => {
+    return storage[key];
+  });
+}
+
 describe('local-database-collection-tests', () => {
   describe('getName', () => {
     test('returns the passed name', () => {
       const collection = new LocalDatabaseCollection('foo');
       expect(collection.getName()).toBe('foo');
-    })
+    });
+  });
+
+  describe('insertItem', () => {
+    beforeEach(() => {
+      initializeLocalStorageMock();
+    });
   });
 });
