ff3e37e4264d34cba44df719a053c2d449b844a1
Jörn Neumeyer <joernneumeyer@protonmail.ch>
2019-03-31 18:25:57 +0200

    implemented item updating acording to spec

--- src/local-database-collection.ts
+++ src/local-database-collection.ts
@@ -29,4 +29,18 @@ export class LocalDatabaseCollection {
   public selectItems<T = any>(predicate: (a: T) => boolean): T[] {
     return this.items.filter(predicate);
   }
+
+  public updateItems<T = any>(predicate: (item: T) => boolean, updater: (item: T) => void): T[] {
+    const updatedItems: T[] = [];
+    for (let i = 0; i < this.items.length; ++i) {
+      if (!predicate(this.items[i])) {
+        continue;
+      }
+      this.items[i] = updater(this.items[i]);
+      updatedItems.push(this.items[i]);
+    }
+    
+    window.localStorage.setItem(this.collectionKey(), JSON.stringify(this.items));
+    return updatedItems;
+  }
 }
