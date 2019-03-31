a3e9d230cfbbb36ecd0b10f6255107a4a34e2c01
Jörn Neumeyer <joernneumeyer@protonmail.ch>
2019-03-31 21:29:58 +0200

    changed spec for getDatabase to be a non-static method

--- test/local-database-manager.spec.ts
+++ test/local-database-manager.spec.ts
@@ -2,27 +2,33 @@ import {LocalDatabaseManager} from '../src/local-database-manager';
 import {LocalDatabase} from '../src';
 
 describe('local-database-manager-tests', () => {
-  describe('getDatabase', () => {
+  describe('getDatabase()', () => {
+    let manager: LocalDatabaseManager;
+
+    beforeAll(() => {
+      manager = new LocalDatabaseManager();
+    });
+
     test('returns a database instance', () => {
-      const database = LocalDatabaseManager.getDatabase('moinsen');
+      const database = manager.getDatabase('moinsen');
       expect(database).toBeInstanceOf(LocalDatabase);
     });
 
     test('passes the provided database name to the database', () => {
-      const database = LocalDatabaseManager.getDatabase('foo');
+      const database = manager.getDatabase('foo');
       expect(database.getName()).toBe('foo');
     });
 
     test('returns the same database instance on multiple calls', () => {
-      const database = LocalDatabaseManager.getDatabase('moinsen');
-      const databaseSecondCall = LocalDatabaseManager.getDatabase('moinsen');
+      const database = manager.getDatabase('moinsen');
+      const databaseSecondCall = manager.getDatabase('moinsen');
       expect(database).toBe(databaseSecondCall);
     });
 
     test('returns the same database instance on multiple calls after switching given database name', () => {
-      const database = LocalDatabaseManager.getDatabase('moinsen');
-      LocalDatabaseManager.getDatabase('foo');
-      const databaseThirdCall = LocalDatabaseManager.getDatabase('moinsen');
+      const database = manager.getDatabase('moinsen');
+      manager.getDatabase('foo');
+      const databaseThirdCall = manager.getDatabase('moinsen');
       expect(database).toBe(databaseThirdCall);
     });
   });
