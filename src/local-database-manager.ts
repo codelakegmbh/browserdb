55ae5f6ec21e359d8fcfe630b30e8120f8df3a4c
Jörn Neumeyer <joernneumeyer@protonmail.ch>
2019-03-31 21:34:22 +0200

    implemented singleton access

--- src/local-database-manager.ts
+++ src/local-database-manager.ts
@@ -2,6 +2,7 @@ import {LocalDatabase} from './local-database';
 
 export class LocalDatabaseManager {
   private readonly instances: LocalDatabase[] = [];
+  private static singletonInstance: LocalDatabaseManager;
 
   public getDatabase(name: string) {
     const existingInstance = this.instances.filter(x => x.getName() === name)[0];
@@ -11,6 +12,13 @@ export class LocalDatabaseManager {
     const newInstance = new LocalDatabase(name);
     this.instances.push(newInstance);
     return newInstance;
+  }
+
+  public static getInstance() {
+    if (!LocalDatabaseManager.singletonInstance) {
+      LocalDatabaseManager.singletonInstance = new LocalDatabaseManager();
+    }
+    return LocalDatabaseManager.singletonInstance;
   }
 }
 
