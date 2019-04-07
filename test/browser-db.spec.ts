54de5432178a3fded009bad8c33b4ca506877a8a
Jöern Neumeyer <joernneumeyer@protonmail.ch>
2019-04-07 19:15:32 +0200

    added spec for browser db property decorator

--- test/browser-db.spec.ts
+++ test/browser-db.spec.ts
@@ -1,4 +1,4 @@
-import {BrowserDb, BrowserDbCollection} from '../src';
+import {BrowserDb, BrowserDbCollection, BrowserDbManager, BrowserDbProp} from '../src';
 
 describe('local-database-tests', () => {
   describe('class BrowserDb', () => {
@@ -56,6 +56,40 @@ describe('local-database-tests', () => {
         const collection2 = db.getCollection('bar');
         expect(collection).not.toBe(collection2);
       });
+    });
+  });
+
+  describe('BrowserDbProp', () => {
+    test('fills the given class property with a BrowserDb instance', () => {
+      class Foo {
+        @BrowserDbProp('foo')
+        public db: BrowserDb;
+      }
+
+      const g = new Foo();
+      expect(g.db).toBeInstanceOf(BrowserDb);
+    });
+
+    test('the db instance has the proper name attached to it', () => {
+      class Foo {
+        @BrowserDbProp('foo')
+        public db: BrowserDb;
+      }
+
+      const g = new Foo();
+      expect(g.db.getName()).toBe('foo');
+    });
+
+    test('assigns a database via the manager singleton', () => {
+      BrowserDbManager.getInstance().clear();
+      const db = BrowserDbManager.getInstance().getDatabase('foo');
+      class Foo {
+        @BrowserDbProp('foo')
+        public db: BrowserDb;
+      }
+
+      const g = new Foo();
+      expect(g.db).toBe(db);
     });
   });
 });
