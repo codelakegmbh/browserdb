45a5d26d329172396d389fbcc50859c5805330de
Jöern Neumeyer <joernneumeyer@protonmail.ch>
2019-04-07 21:44:49 +0200

    moved localStorage mock into separate file

--- test/browser-db-collection.spec.ts
+++ test/browser-db-collection.spec.ts
@@ -1,18 +1,5 @@
 import {BrowserDbCollection, BrowserDb} from '../src';
-
-function initializeLocalStorageMock() {
-  let storage: { [key: string]: string } = {};
-
-  spyOn(window.localStorage, 'setItem').and.callFake((key: string, value: string) => {
-    storage[key] = value;
-  });
-
-  spyOn(window.localStorage, 'getItem').and.callFake((key: string) => {
-    return storage[key];
-  });
-
-  return storage;
-}
+import {initializeLocalStorageMock} from './utilities';
 
 let collection: BrowserDbCollection;
 
