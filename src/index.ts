b8e978b1b6535e099d30ffb19cb5534748be549d
Jöern Neumeyer <joernneumeyer@protonmail.ch>
2019-04-10 23:14:54 +0200

    added export for collection props

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import { BrowserDb } from './browser-db';
 import { BrowserDbCollection } from './browser-db-collection';
+import { BrowserDbCollectionProp } from './browser-db-collection-prop';
 import { BrowserDbManager } from './browser-db-manager';
 import { BrowserDbProp } from './browser-db-prop';
 
 export {
   BrowserDb,
   BrowserDbCollection,
+  BrowserDbCollectionProp,
   BrowserDbManager,
   BrowserDbProp,
 };
