50b6103c9693f7c57ee2b96163a3a43a8f7caa85
Jöern Neumeyer <joernneumeyer@protonmail.ch>
2019-04-10 23:15:12 +0200

    added spec for collection props

--- test/browser-db-collection-prop.spec.ts
+++ test/browser-db-collection-prop.spec.ts
@@ -1,5 +1,30 @@
+import { BrowserDbCollectionProp, BrowserDbCollection, BrowserDbManager } from '../src';
+
 describe('browser-db-collection-prop-tests', () => {
-  test('dummy', () => {
-    
+  class Foo {
+    @BrowserDbCollectionProp('foo', 'bar')
+    public collection: BrowserDbCollection;
+  }
+
+  test('fills the given class property with a BrowserDbCollection instance', () => {
+    const g = new Foo();
+    expect(g.collection).toBeInstanceOf(BrowserDbCollection);
+  });
+
+  test('the collection instance has the proper name attached to it', () => {
+    const g = new Foo();
+    expect(g.collection.getName()).toBe('bar');
+  });
+
+  test('the database of the collection instance has the proper name attached to it', () => {
+    const g = new Foo();
+    expect(g.collection.getDatabase().getName()).toBe('foo');
+  });
+
+  test('assigns a database via the manager singleton', () => {
+    const collection = BrowserDbManager.getInstance().getDatabase('foo').getCollection('bar');
+
+    const g = new Foo();
+    expect(g.collection).toBe(collection);
   });
 });
