dd12f259799cdc74d5e8cdac66d77dd4e7aa45c0
Jöern Neumeyer <joernneumeyer@protonmail.ch>
2019-04-10 23:21:25 +0200

    implemented collection property according to spec

--- src/browser-db-collection-prop.ts
+++ src/browser-db-collection-prop.ts
@@ -1,4 +1,10 @@
+import {BrowserDbManager} from '.';
 
-export function BrowserDbCollectionProp() {
-
+export function BrowserDbCollectionProp(databaseName: string, collectionName: string) {
+  return function(target: any, propertyName: string) {
+    target[propertyName] = BrowserDbManager
+      .getInstance()
+      .getDatabase(databaseName)
+      .getCollection(collectionName);
+  };
 }
