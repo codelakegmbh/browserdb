35008b91fd09f31b14c96b95ece2d609dd82424d
Jöern Neumeyer <joernneumeyer@protonmail.ch>
2019-04-14 16:43:39 +0200

    added spec for updating; added spec for listener adding

--- test/browser-db-collection-event.spec.ts
+++ test/browser-db-collection-event.spec.ts
@@ -9,6 +9,12 @@ describe('browser-db-collection-event-tests', () => {
     collection = db.getCollection('bar');
   });
 
+  describe('on', () => {
+    test('does not fail on invalid event', () => {
+      expect(() => collection.on('dfghedt' as any, () => { })).not.toThrow();
+    });
+  });
+
   describe('insertion', () => {
     test('triggers on event', () => {
       let works = false;
@@ -31,6 +37,43 @@ describe('browser-db-collection-event-tests', () => {
       const cb = (a: number, col: BrowserDbCollection) => works = col === collection;
       collection.on('insert', cb);
       collection.insertItem(1);
+      expect(works).toBeTruthy();
+    });
+  });
+
+  describe('update', () => {
+    test('triggers on event', () => {
+      let works = false;
+      const cb = () => works = true;
+      collection.insertItem(1);
+      collection.on('update', cb);
+      collection.updateItems(() => true, () => 2);
+      expect(works).toBeTruthy();
+    });
+
+    test('does not trigger if no items were updated', () => {
+      let works = true;
+      const cb = () => works = false;
+      collection.on('update', cb);
+      collection.updateItems(() => true, () => 2);
+      expect(works).toBeTruthy();
+    });
+
+    test('passes the updated items to the callback', () => {
+      let numbers: number[] = [];
+      const cb = (a: number[]) => numbers = a;
+      collection.insertItem(1);
+      collection.on('update', cb);
+      collection.updateItems(() => true, () => 2);
+      expect(numbers).toEqual([2]);
+    });
+
+    test('passes the collection to the callback', () => {
+      let works = false;
+      const cb = (a: number[], col: BrowserDbCollection) => works = col === collection;
+      collection.insertItem(1);
+      collection.on('update', cb);
+      collection.updateItems(() => true, () => 2);
       expect(works).toBeTruthy();
     });
   });
