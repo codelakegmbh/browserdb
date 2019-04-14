c1f5b70cb36263b26f6da61d248bc62bacda9ff6
Jöern Neumeyer <joernneumeyer@protonmail.ch>
2019-04-14 17:10:17 +0200

    changed spec to require async callback inkokation

--- test/browser-db-collection-event.spec.ts
+++ test/browser-db-collection-event.spec.ts
@@ -17,65 +17,81 @@ describe('browser-db-collection-event-tests', () => {
   });
 
   describe('insertion', () => {
-    test('triggers on event', () => {
+    test('triggers on event async', (done) => {
       let works = false;
-      const cb = () => works = true;
+      const cb = () => {
+        works = true;
+        expect(works).toBeTruthy();
+        done();
+      };
       collection.on('insert', cb);
       collection.insertItem(1);
-      expect(works).toBeTruthy();
+      expect(works).toBe(false);
     });
 
-    test('passes the new item to the callback', () => {
-      let works = false;
-      const cb = (a: number) => works = a === 1;
+    test('passes the new item to the callback', (done) => {
+      const cb = (a: number) => {
+        expect(a).toBe(1);
+        done();
+      };
       collection.on('insert', cb);
       collection.insertItem(1);
-      expect(works).toBeTruthy();
     });
 
-    test('passes the collection to the callback', () => {
+    test('passes the collection to the callback', (done) => {
       let works = false;
-      const cb = (a: number, col: BrowserDbCollection) => works = col === collection;
+      const cb = (a: number, col: BrowserDbCollection) => {
+        expect(col).toBe(collection);
+        done();
+      };
       collection.on('insert', cb);
       collection.insertItem(1);
-      expect(works).toBeTruthy();
     });
   });
 
   describe('update', () => {
-    test('triggers on event', () => {
+    test('triggers on event async', (done) => {
       let works = false;
-      const cb = () => works = true;
+      const cb = () => {
+        works = true;
+        expect(works).toBeTruthy();
+        done();
+      };
       collection.insertItem(1);
       collection.on('update', cb);
       collection.updateItems(() => true, () => 2);
-      expect(works).toBeTruthy();
+      expect(works).toBe(false);
     });
 
-    test('does not trigger if no items were updated', () => {
+    test('does not trigger if no items were updated', (done) => {
       let works = true;
       const cb = () => works = false;
       collection.on('update', cb);
       collection.updateItems(() => true, () => 2);
-      expect(works).toBeTruthy();
+      setTimeout(() => {
+        expect(works).toBeTruthy();
+        done();
+      }, 20);
     });
 
-    test('passes the updated items to the callback', () => {
-      let numbers: number[] = [];
-      const cb = (a: number[]) => numbers = a;
+    test('passes the updated items to the callback', (done) => {
+      const cb = (numbers: number[]) => {
+        expect(numbers).toEqual([2]);
+        done();
+      };
       collection.insertItem(1);
       collection.on('update', cb);
       collection.updateItems(() => true, () => 2);
-      expect(numbers).toEqual([2]);
     });
 
-    test('passes the collection to the callback', () => {
-      let works = false;
-      const cb = (a: number[], col: BrowserDbCollection) => works = col === collection;
+    test('passes the collection to the callback', (done) => {
+      const cb = (a: number[], col: BrowserDbCollection) => {
+        expect(col).toBe(collection);
+        done();
+      };
       collection.insertItem(1);
       collection.on('update', cb);
       collection.updateItems(() => true, () => 2);
-      expect(works).toBeTruthy();
     });
   });
 });
